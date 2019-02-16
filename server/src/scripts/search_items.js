613fc12506960092a3674d9e0e7277d0a1978300
Anirudh Mungre <anirudh.mungre16@gmail.com>
2019-02-16 17:57:35 -0500

    item index list

---
+++ server/src/scripts/search_items.js
@@ -0,0 +1,19 @@
+"use strict"
+
+const search_items = async (imageName) => {
+    let items = [];
+    // Imports the Google Cloud client library
+    const vision = require('@google-cloud/vision');
+
+    // Creates a client
+    const client = new vision.ImageAnnotatorClient();
+
+    // Performs label detection on the image file
+    const [result] = await client.labelDetection('../resources/' + imageName + '.jpg');
+    const labels = result.labelAnnotations;
+    labels.forEach(label => items.push(label.description));
+    console.log(items);
+    return items;
+}
+
+exports.search_items = search_items
