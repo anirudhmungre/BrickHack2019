e2975f251ff3808be5ff119214722fb87294fa6e
Anirudh Mungre <anirudh.mungre16@gmail.com>
2019-02-17 01:02:58 -0500

    list of searched items

--- server/src/scripts/search_results.js
+++ server/src/scripts/search_results.js
@@ -3,31 +3,26 @@ const { search_items } = require("./search_items")
 const fetch = require("node-fetch")
 const { URL, URLSearchParams } = require('url');
 
-search_items("General-Tao").then(token => {
-    const base_url = new URL("https://api.wegmans.io")
+const items_prices = async (img_name) => {
+    const query_list = await search_items(img_name)
+    const query_str = query_list.join("|")
+
     const search_url = new URL("https://api.wegmans.io/products/search")
-    console.log(token.join("|"))
 
     const params = {
         "Subscription-Key": "c59dd53a607f42ae830de7469a83a985",
-        "query": token.join("|"),
+        "query": query_str,
         "api-version": "2018-10-18",
         method: "POST"
     }
     search_url.search = new URLSearchParams(params)
-    fetch(search_url)
-        .then(data => { return data.json() })
-        .then(res => {
-            // console.log(res.results)
-            res.results.forEach(result => {
-                console.log(result.name)
-                // 0 -> self
-                // 1 -> locations
-                // 2 -> availabilities
-                // 3 -> prices
-                console.log(base_url.origin + result._links[2].href)
-                console.log(base_url.origin + result._links[3].href)
-            });
-        })
-        .catch(error => console.log(error))
-})
+    const response = await fetch(search_url)  
+    const json = await response.json()
+    let items = {}
+    json.results.forEach(result => {
+        items[result.sku] = result.name
+    })
+    return items
+}
+
+exports.items_prices = items_prices
