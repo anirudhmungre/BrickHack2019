5be4abc59ed189d57ebd538dc51f47eb848c201b
Anirudh Mungre <anirudh.mungre16@gmail.com>
2019-02-16 21:50:09 -0500

    linking

--- server/src/scripts/search_results.js
+++ server/src/scripts/search_results.js
@@ -3,19 +3,31 @@ const { search_items } = require("./search_items")
 const fetch = require("node-fetch")
 const { URL, URLSearchParams } = require('url');
 
-search_items("oranges").then(token => {
-    const url = new URL("https://api.wegmans.io/products/search")
-    console.log(token.join("|"))   
+search_items("General-Tao").then(token => {
+    const base_url = new URL("https://api.wegmans.io")
+    const search_url = new URL("https://api.wegmans.io/products/search")
+    console.log(token.join("|"))
 
     const params = {
-        "Subscription-Key":"c59dd53a607f42ae830de7469a83a985",
+        "Subscription-Key": "c59dd53a607f42ae830de7469a83a985",
         "query": token.join("|"),
-        "api-version":"2018-10-18",
+        "api-version": "2018-10-18",
         method: "POST"
     }
-    url.search = new URLSearchParams(params)
-    fetch(url)
-    .then(data=>{return data.json()})
-    .then(res=>{console.log(res)})
-    .catch(error=>console.log(error))
+    search_url.search = new URLSearchParams(params)
+    fetch(search_url)
+        .then(data => { return data.json() })
+        .then(res => {
+            // console.log(res.results)
+            res.results.forEach(result => {
+                console.log(result.name)
+                // 0 -> self
+                // 1 -> locations
+                // 2 -> availabilities
+                // 3 -> prices
+                console.log(base_url.origin + result._links[2].href)
+                console.log(base_url.origin + result._links[3].href)
+            });
+        })
+        .catch(error => console.log(error))
 })
